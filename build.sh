b2e2b241a4757266a050cba209744304c2f07337
unknown <vaibhavagarwaleverest@gmail.com>
2025-01-19 05:46:09 +0530

    added buildsh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version  9.0.
 export PATH="$PATH:/opt/render/project/src/.dotnet"
 
 # Install Python dependencies
-pip install -r backend/requirements.txt
+pip install -r backend/requirement.txt
