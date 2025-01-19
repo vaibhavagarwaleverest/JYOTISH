164b41c27ba45242417218b4a09849039e4e4660
unknown <vaibhavagarwaleverest@gmail.com>
2025-01-19 05:42:47 +0530

    added buildsh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 6.0.0 --install-dir /opt/render/project/src/.dotnet
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 7.0.20 --install-dir /opt/render/project/src/.dotnet
 
 # Add .NET to PATH
 export PATH="$PATH:/opt/render/project/src/.dotnet"
