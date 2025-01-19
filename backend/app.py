3d9380fd01fbd3d7368ce89fe78a0eba4a0402c4
unknown <vaibhavagarwaleverest@gmail.com>
2025-01-19 09:07:34 +0530

    added reqjson

--- backend/app.py
+++ backend/app.py
@@ -19,11 +19,12 @@ client = DataAPIClient(astra_db_token)
 database = client.get_database(astra_db_endpoint)
 collection = database.get_collection(astra_db_collection)
 app = Flask(__name__)
-CORS(app)
+CORS(app,resources={r"/*": {"origins": "*", "allow_headers": ["Content-Type", "Token"]}})
 def flatten_dict(d, parent_key='', sep=' '):
     """
     Flatten a nested dictionary into a single-level dictionary.
     """
+
     items = []
     for k, v in d.items():
         new_key = f"{parent_key}{sep}{k}" if parent_key else k
